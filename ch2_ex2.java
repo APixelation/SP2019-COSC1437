3b6eeaf68192660998b8ebfb99601a2a60d9c9c6
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-01-31 22:22:54 +0900

    Completion of Chapter 2 Exercise 2 Java Program

---
+++ ch2_ex2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ch2_ex2
+{
+    public static void main(String[] args)
+    {
+        int num1, num2, num3, average;
+        num1 = 125;
+        num2 = 28;
+        num3 = -25;
+
+        average = (num1 + num2 + num3) / 3;
+        System.out.print("The value of num1: " + num1 + ", num2: " + num2 + ", num3: " + num3 +
+        " and the average of these 3 numbers are " + average + ".");
+    }
+}
