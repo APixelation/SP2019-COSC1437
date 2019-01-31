747a3f3e126a00f755e2c90bdacc188643014f41
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-01-31 21:38:29 +0900

    Created exampe Java program.

---
+++ ex2_27a.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class ex2_27a
+{
+    static Scanner console = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        int num;
+        double height;
+        String name;
+
+        System.out.print("Enter an integer: ");
+        num = console.nextInt();
+        System.out.println();
+
+        System.out.println("num: " + num);
+
+        System.out.print("Enter first name: ");
+        name = console.next();
+        System.out.println();
+
+        System.out.print("Enter height: ");
+        height = console.nextDouble();
+        System.out.println();
+
+        system.out.println("Height: " + height);
+    }
+}
