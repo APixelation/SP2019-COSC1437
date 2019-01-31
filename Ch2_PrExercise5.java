830e96c034d5da45a76c1c82271fcf11b9ae2a8f
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-01-31 22:37:36 +0900

    Rearranged the program in the correct order, tested works.

--- Ch2_PrExercise5.java
+++ Ch2_PrExercise5.java
@@ -1,29 +1,29 @@
+import java.util.*;
+
 public class Ch2_PrExercise5
 {
     static Scanner console = new Scanner(System.in);
-
-    import java.util.*;
+    
+    public static void main (String[] args)
     {
-        public static void main (String[] args)
         int width;
+        int length;
+        int area;
+        int perimeter;
 
         System.out.print("Enter the length: ");
         width = console.nextInt();
         System.out.println();
-        int length;
 
         System.out.print("Enter the width: ");
         length = console.nextInt();
-
         System.out.println();
 
         area = length * width;
+        perimeter = 2 * (length + width);
 
         System.out.println("Area = " + area);
         System.out.println("Perimeter = " + perimeter);
-        perimeter = 2 * (length + width);
 
-        int area;
-        int perimeter;
     }
 }
