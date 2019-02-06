094b72328d2bd1dc74f90867ebc540364703ca8a
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-02-06 18:59:51 +0900

    Added into to program example.

---
+++ ch3_prog_ex.java
@@ -0,0 +1,9 @@
+// Program : Movie ticket sale and donation to charity.
+// This program prompts the user to input the movie name, adult
+// ticket price, child ticket price, number of adult tickets
+// sold, number of child tickets sold, and the percentage of the
+// gross amount to be donated to the charity.
+// The program outputs the movie name, the number of tickets
+// sold, the gross amount, the percentage of the gross amount
+// donated to the charity, the amount donated to the charity,
+// and the net amount.
