8b30069cc7477d01741b421c6ac7ef4a5ebb51d6
Kaven Chanthaphy <kavenchanthaphy@outlook.com>
2019-02-06 19:26:32 +0900

    Set up initial required files for the student average calculator

---
+++ ch3_ex_student_grade.java
@@ -0,0 +1,15 @@
+import java.io*;
+import java.util.*;
+
+public class ch3_ex_student_grade
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        double test1, test2, test3, test4, test5, average;
+        String firstName, lastName;
+
+        Scanner inFile = new Scanner(new FileReader("ch3_ex_sg_test.txt"));
+
+        PrintWriter outFile = new PrintWriter("ch3_ex_sg_testavg.out");
+    }
+}
